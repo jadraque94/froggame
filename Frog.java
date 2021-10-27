c282899dfd8d00ac17637b288c51e3a7e4255311
jadraque94 <91532800+jadraque94@users.noreply.github.com>
2021-10-27 21:20:26 +0200

    Add files via upload

---
+++ Frog.java
@@ -0,0 +1,16 @@
+package org.openjdk.gamefrogger;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Frog extends Application {
+
+    public static void main(String[] args) {
+        launch( args );
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
